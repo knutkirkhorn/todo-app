3777b3f9d7720787fbdab881b8596335f823f1d3
Knut Kirkhorn <knutkirk@hotmail.com>
2018-10-19 18:34:00 +0200

    Add test for `Do` option

--- ToDoAppTests/TodoAppTests.cs
+++ ToDoAppTests/TodoAppTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ToDoApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.IO;
 
@@ -37,6 +38,27 @@ namespace ToDoApp.Tests
                 todoApp.Print();
 
                 Assert.AreEqual(expectedPrint, sw.ToString());
+            }
+        }
+
+        [TestMethod()]
+        public void DoTest()
+        {
+            TodoApp todoApp = new TodoApp();
+            todoApp.UseTestEnvironment();
+
+            // Retrieve output to command line, redirect it to the String Writer
+            using (StringWriter sw = new StringWriter())
+            {
+                // Set the console to print to sw
+                Console.SetOut(sw);
+
+                todoApp.Add("This is a test");
+                todoApp.Add("another string");
+                todoApp.Do(1);
+                string expected = "#1 This is a test\r\n#2 another string\r\nCompleted #1 This is a test\r\n";
+
+                Assert.AreEqual(expected, sw.ToString());
             }
         }
     }
